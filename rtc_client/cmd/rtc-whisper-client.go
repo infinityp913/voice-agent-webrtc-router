25c10cad6b6458c75d1e6bb19a93868515cf2649
Ananth Preetham <infinityp913@gmail.com>
2023-07-31 11:52:07 -0400

    extracted ria_client into its own file

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -9,102 +9,11 @@ import (
 	"github.com/GRVYDEV/S.A.T.U.R.D.A.Y/stt/engine"
 	stt "github.com/GRVYDEV/S.A.T.U.R.D.A.Y/stt/engine"
 	"github.com/infinityp913/rtc-go-server/rtc_client"
-	"github.com/pion/webrtc/v3"
 )
 
 var (
 	logger = logr.New()
 )
-
-type RiaConfig struct {
-	// ION room name to connect to
-	Room string
-	// URL for websocket server
-	Url url.URL
-	// STT engine to generate transcriptions
-	SttEngine *stt.Engine
-
-	// channel used to send transcription segments over the data channel
-	// any transcription segment sent on this channel with be sent over the data channel
-	TranscriptionStream chan stt.Document
-}
-
-type RiaClient struct {
-	ws     *rtc_client.SocketConnection
-	rtc    *RTCConnection
-	config RiaConfig
-	ae     *rtc_client.AudioEngine
-}
-
-func NewRiaClient(config RiaConfig) (*RiaClient, error) {
-	ae, err := rtc_client.NewAudioEngine(config.SttEngine)
-	if err != nil {
-		return nil, err
-	}
-
-	ws := rtc_client.NewSocketConnection(config.Url)
-
-	rtc, err := NewRTCConnection(RTCConnectionParams{
-		trickleFn: func(candidate *webrtc.ICECandidate, target int) error {
-			return ws.SendTrickle(candidate, target)
-		},
-		rtpChan: ae.RtpIn(),
-		// transcriptionStream: config.TranscriptionStream,
-		transcriptionStream: nil,
-		mediaIn:             ae.MediaOut(),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	s := &RiaClient{
-		ws:     ws,
-		rtc:    rtc,
-		config: config,
-		ae:     ae,
-	}
-
-	s.ws.SetOnOffer(s.OnOffer)
-	s.ws.SetOnAnswer(s.OnAnswer)
-	s.ws.SetOnTrickle(s.rtc.OnTrickle)
-
-	return s, nil
-}
-
-func (s *RiaClient) OnAnswer(answer webrtc.SessionDescription) error {
-	return s.rtc.SetAnswer(answer)
-}
-
-func (s *RiaClient) OnOffer(offer webrtc.SessionDescription) error {
-	ans, err := s.rtc.OnOffer(offer)
-	if err != nil {
-		logger.Error(err, "error getting answer")
-		return err
-	}
-
-	return s.ws.SendAnswer(ans)
-}
-
-func (s *RiaClient) Start() error {
-	if err := s.ws.Connect(); err != nil {
-		logger.Error(err, "error connecting to websocket")
-		return err
-	}
-	offer, err := s.rtc.GetOffer()
-	if err != nil {
-		logger.Error(err, "error getting intial offer")
-	}
-	if err := s.ws.Join(s.config.Room, offer); err != nil {
-		logger.Error(err, "error joining room")
-		return err
-	}
-
-	// s.ae.Start()
-
-	s.ws.WaitForDone()
-	logger.Info("Socket done goodbye")
-	return nil
-}
 
 func main() {
 	url := url.URL{Scheme: "ws", Host: "matherium.com", Path: "/go-server"}
@@ -127,7 +36,7 @@ func main() {
 		UseVad:           true,
 	})
 
-	sc, err := NewRiaClient(RiaConfig{
+	sc, err := rtc_client.NewRiaClient(rtc_client.RiaConfig{
 		Room:                "",
 		Url:                 url,
 		SttEngine:           sttEngine,
