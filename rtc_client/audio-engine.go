c56de71826d0aa201bcf0a2e9b46e399abb2320e
Ananth Preetham <infinityp913@gmail.com>
2023-09-01 16:40:52 -0400

    chunked the pcm_arr from the post req

--- rtc_client/audio-engine.go
+++ rtc_client/audio-engine.go
@@ -103,9 +103,30 @@ func NewAudioEngine(sttEngine *stt.Engine) (*AudioEngine, error) {
 	var pcm_arr []float32 = flaskResponse.Pcm_arr
 	internal.Logger.Info("len(pcm_arr): ", len(pcm_arr))
 
+	// Chunking pcm_arr before passing to ae.Encode()
+	var chunked_pcm_arr [][]float32
+
+	chunksize := 4800
+
+	for i := 0; i < len(pcm_arr); i += chunksize {
+		end := i + chunksize
+
+		if end > len(pcm_arr) {
+			end = len(pcm_arr)
+		}
+		chunked_pcm_arr = append(chunked_pcm_arr, pcm_arr[i:end])
+
+	}
+
 	internal.Logger.Info("before encode") // REMOVE AFTER DEBUG
-	// pass it to ae.Encode()
-	ae.Encode(pcm_arr, 1, 22050)
+
+	// Looping through the chunks
+	for _, chunk := range chunked_pcm_arr {
+		// pass it to ae.Encode(), where the pcm array is encoded to Opus frames AND
+		// they're sent over to the browser via WebRTC using the processOutgoingMedia() function in AudioEngine
+		ae.Encode(chunk, 1, 22050)
+	}
+
 	internal.Logger.Info("After encode") // REMOVE AFTER DEBUG
 
 	return ae, nil
