8c2bacabd32d71a8306bacf3417211606cab9ad4
Ananth Preetham <infinityp913@gmail.com>
2023-09-19 22:56:53 -0400

    added single quotes around currentPrompt

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -171,7 +171,8 @@ func (p *PromptBuilder) tryCallEngine(ae *rtc_client.AudioEngine, rtc *rtc_clien
 	logger.Info("Getting PCM data from Flask Server") // REMOVE AFTER DEBUG
 	url := "http://localhost:8000/get_response"       // Flask server running QnA NN + TTS NN is hosted here
 	logger.Info("currentPrompt: ", currentPrompt)     // REMOVE AFTER DEBUG
-	jsonStr := `{'end_user_input': ` + currentPrompt + `, 'curr_state':'4', 'client_id':'1', 'prompt_repeated_response':'0'}`
+	jsonStr := `{'end_user_input': '` + currentPrompt + `', 'curr_state':'4', 'client_id':'1', 'prompt_repeated_response':'0'}`
+	// jsonStr := `{'text': ` + currentPrompt + `, 'curr_state':'4', 'client_id':'1', 'prompt_repeated_response':'0'}`
 	var jsonStrByte = []byte(jsonStr)
 
 	flaskResponse := new(FlaskResponse)
