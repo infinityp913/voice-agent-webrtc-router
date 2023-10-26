d846252725b188847b513180c60eae70fecc4900
Ananth Preetham <infinityp913@gmail.com>
2023-10-26 15:10:31 -0400

    putting rtc.unlock and lock back around POM

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -209,9 +209,9 @@ func (p *PromptBuilder) Start(ae *rtc_client.AudioEngine, rtc *rtc_client.RTCCon
 		// wait for the timer to fire OR Stop() to be called
 		select {
 		case <-p.timer.C: // indicates firing of timer aka the 2s timer has counted down
-			p.Lock()
+			// p.Lock()
 			p.tryCallEngine(ae, rtc)
-			p.Unlock()
+			// p.Unlock()
 		case <-p.cancel: // indicates calling of Stop()
 			logger.Info("shutting down llm interface")
 			return
@@ -318,11 +318,11 @@ func (p *PromptBuilder) tryCallEngine(ae *rtc_client.AudioEngine, rtc *rtc_clien
 
 	logger.Info("after encode") // REMOVE AFTER DEBUG
 
-	// rtc.Lock()
+	rtc.Lock()
 
 	go rtc.ProcessOutgoingMedia()
 
-	// rtc.Unlock()
+	rtc.Unlock()
 
 	// *** End of sending currentPrompt to Flask server code ***
 
