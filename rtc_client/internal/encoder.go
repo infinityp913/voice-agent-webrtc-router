67ddb2f3810b34e7535183945faca0f4bdf948cb
Ananth Preetham <infinityp913@gmail.com>
2023-11-20 19:04:10 -0500

    put the go back in encoder.encode and added a 20ms sleep between iterations

--- rtc_client/internal/encoder.go
+++ rtc_client/internal/encoder.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	logr "github.com/GRVYDEV/S.A.T.U.R.D.A.Y/log"
 	"github.com/GRVYDEV/S.A.T.U.R.D.A.Y/util"
@@ -78,12 +79,13 @@ func (o *OpusEncoder) Encode(pcm []float32, inputChannelCount, inputSampleRate i
 	// }
 	opusFrames := make([]OpusFrame, len(frames)) // made the opusFrames a slice of fixed length and capacity, cap=len to enable indexing below
 	for idx, frame := range frames {
-		func(idx int, frame PcmFrame) {
+		go func(idx int, frame PcmFrame) {
 			opusFrame, _ := o.encodeToOpus(frame)
 			// if err != nil {
 			// 	Logger.Error(err, "error encoding opus frame") // RISK: WE'RE NOT RETURNING THE ERROR OVER HERE
 			// }
 			opusFrames[idx] = opusFrame // Since all goroutines write to different memory locations (coz of indexing) this isn't racy. [inspiration: https://stackoverflow.com/questions/18499352/golang-concurrency-how-to-append-to-the-same-slice-from-different-goroutines]
+			time.Sleep(time.Millisecond * 20)
 		}(idx, frame)
 	}
 
