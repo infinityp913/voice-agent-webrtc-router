b8ea36e8861772f9a67d1d66238b2d7ff19e4dec
Ananth Preetham <infinityp913@gmail.com>
2023-11-29 19:27:13 -0500

    added a 65s delay before colsd

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -127,6 +127,7 @@ func main() {
 	logger.Info("SENT SIGNAL TO START BROWSER CLIENT")
 	// Done sending signal to start browser client
 
+	time.Sleep(5000 * time.Millisecond) // NOV 29 -- LET THE BROWSER CLIENT get set up
 	// COMMENTED NOV 28
 	if err := rc.CreateOfferAndSetLocalDescription(); err != nil {
 		logger.Fatal(err, "error creating offer")
