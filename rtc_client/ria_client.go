3ebf44d44aeaa35792a6dd815a65df80758ea195
Ananth Preetham <infinityp913@gmail.com>
2023-12-01 21:13:47 -0500

    removed ws.connect from newriaclient

--- rtc_client/ria_client.go
+++ rtc_client/ria_client.go
@@ -45,7 +45,7 @@ func NewRiaClient(config RiaConfig) (*RiaClient, error) {
 
 	ws := NewSocketConnection(config.Url)
 	// NOV 27
-	ws.Connect()
+	// ws.Connect()
 
 	rtc, err := NewRTCConnection(RTCConnectionParams{
 		trickleFn: func(candidate *webrtc.ICECandidate, target int) error {
