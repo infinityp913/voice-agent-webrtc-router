168938837839565c9db4f724a3473048cb535811
Ananth Preetham <infinityp913@gmail.com>
2023-12-11 13:39:22 -0500

    added mutex locking and unlocking to encodeToOpus

--- rtc_client/internal/encoder.go
+++ rtc_client/internal/encoder.go
@@ -106,6 +106,9 @@ func (o *OpusEncoder) Encode(pcm []float32, inputChannelCount, inputSampleRate i
 }
 
 func (o *OpusEncoder) encodeToOpus(frame PcmFrame) (OpusFrame, error) {
+	var mu sync.Mutex
+
+	mu.Lock()
 	opusFrame := OpusFrame{Index: frame.index}
 	data := make([]byte, 1000)
 
@@ -115,7 +118,7 @@ func (o *OpusEncoder) encodeToOpus(frame PcmFrame) (OpusFrame, error) {
 		return opusFrame, err
 	}
 	opusFrame.Data = data[:n]
-
+	mu.Unlock()
 	return opusFrame, nil
 }
 
