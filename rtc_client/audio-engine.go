13607acb63251d2d4c3d073c9642754e3408898f
Ananth Preetham <infinityp913@gmail.com>
2023-12-17 00:09:21 -0500

    changed 20 to 40

--- rtc_client/audio-engine.go
+++ rtc_client/audio-engine.go
@@ -25,7 +25,7 @@ import (
 const (
 	sampleRate  = stt.SampleRate // (16000)
 	channels    = 1              // decode into 1 channel since that is what whisper.cpp wants
-	frameSizeMs = 20
+	frameSizeMs = 40
 )
 
 var frameSize = channels * frameSizeMs * sampleRate / 1000
@@ -224,7 +224,7 @@ func (a *AudioEngine) sendMedia(frames []internal.OpusFrame) {
 		sample := convertOpusToSample(f)
 		a.mediaOut <- sample
 		// this is important to properly pace the samples
-		time.Sleep(time.Millisecond * 20)
+		time.Sleep(time.Millisecond * 40)
 	}
 	internal.Logger.Info("DEBUG: End of sendMedia")
 }
@@ -233,7 +233,7 @@ func convertOpusToSample(frame internal.OpusFrame) media.Sample {
 	return media.Sample{
 		Data:               frame.Data,
 		PrevDroppedPackets: 0, // FIXME support dropping packets
-		Duration:           time.Millisecond * 20,
+		Duration:           time.Millisecond * 40,
 	}
 }
 
