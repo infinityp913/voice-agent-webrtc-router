f09761f60b8f9dc0bb9be9d1557e10788376c44f
Ananth Preetham <infinityp913@gmail.com>
2023-12-25 22:06:54 -0500

    changed pipe:1 to pipe: for output

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -516,7 +516,7 @@ func riaSaysHello(ae *rtc_client.AudioEngine, rtc *rtc_client.RTCConnection) int
 	logger.Info("Running ffmpeg")
 	err = ffmpeg.Input("pipe:0").
 		WithInput(fd).
-		Output("pipe:1", ffmpeg.KwArgs{"c:a": "libopus", "page_duration": 2000, "ac": 2, "f": "s16le"}).
+		Output("pipe:", ffmpeg.KwArgs{"c:a": "libopus", "page_duration": 2000, "ac": 2, "f": "s16le"}).
 		Run()
 	if err != nil {
 		logger.Info("Error at ffmpeg.Input()!!", err)
