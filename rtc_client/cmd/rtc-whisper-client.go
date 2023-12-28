2a30321e389e3f71727a0112f0cfd6f8bfbd5cd3
Ananth Preetham <infinityp913@gmail.com>
2023-12-27 19:33:29 -0500

    writing to file before and afrter resampling

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -569,10 +569,16 @@ func riaSaysHello(ae *rtc_client.AudioEngine, rtc *rtc_client.RTCConnection) int
 	logger.Info("Length of opus_byte_arr: ", len(opus_byte_arr))
 	outBuf.Reset()
 
+	// write the opus_byte_arr to a file
+	err = ioutil.WriteFile("opus_byte_arr.ogg", opus_byte_arr, 0644)
+	if err != nil {
+		logger.Info("Error writing to file:", err)
+	}
+
 	opus_byte_arr = ResampleByte(opus_byte_arr, 22050, 48000)
 
 	// write the opus_byte_arr to a file
-	err = ioutil.WriteFile("opus_byte_arr.ogg", opus_byte_arr, 0644)
+	err = ioutil.WriteFile("opus_byte_arr48KHz.ogg", opus_byte_arr, 0644)
 	if err != nil {
 		logger.Info("Error writing to file:", err)
 	}
