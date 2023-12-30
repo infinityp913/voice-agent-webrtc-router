5159950bbfa947981f77a9c0138b707327c094db
Ananth Preetham <infinityp913@gmail.com>
2023-12-30 18:33:04 -0500

    increased sleep before riaSayshello to 1s

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -130,7 +130,7 @@ func main() {
 		logger.Fatal(err, "error creating offer")
 	} //NOV 28
 
-	time.Sleep(600 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 
 	init_state := riaSaysHello(rc.Ae, rc.Rtc)
 
