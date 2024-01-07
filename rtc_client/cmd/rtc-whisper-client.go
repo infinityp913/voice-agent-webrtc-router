f42ab298e68672884bf8d4c0b89eefa8529ede78
Ananth Preetham <infinityp913@gmail.com>
2024-01-07 17:47:00 -0500

    chanegd to whisper base

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -85,7 +85,7 @@ func main() {
 
 	url := url.URL{Scheme: "wss", Host: "matherium.com", Path: "/go-server"}
 
-	whisperCpp, err := whisper.New("../models/ggml-tiny.en.bin")
+	whisperCpp, err := whisper.New("../models/ggml-base.en.bin")
 	if err != nil {
 	}
 
