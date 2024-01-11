3daa6be7a5b3e058a4e870b5c36fce903e6f10e1
Ananth Preetham <infinityp913@gmail.com>
2024-01-11 15:12:28 -0500

    divided pcm and buf size by 2

--- rtc_client/audio-engine.go
+++ rtc_client/audio-engine.go
@@ -113,8 +113,8 @@ func NewAudioEngine(sttEngine *stt.Engine) (*AudioEngine, error) {
 	ae := &AudioEngine{
 		rtpIn:          make(chan *rtp.Packet),
 		mediaOut:       make(chan media.Sample),
-		pcm:            make([]float32, 20),
-		buf:            make([]byte, 20*2),
+		pcm:            make([]float32, frameSize/2),
+		buf:            make([]byte, frameSize/2*2),
 		dec:            dec,
 		enc:            enc,
 		sttEngine:      sttEngine,
