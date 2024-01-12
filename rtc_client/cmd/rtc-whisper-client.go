2dfd67566eff71712627969ac1e3fef4c8de66db
Ananth Preetham <infinityp913@gmail.com>
2024-01-12 10:59:33 -0500

    increased delay before intro to 2900

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -132,7 +132,7 @@ func main() {
 		logger.Fatal(err, "error creating offer")
 	} //NOV 28
 
-	time.Sleep(2500 * time.Millisecond)
+	time.Sleep(2900 * time.Millisecond)
 
 	init_state := riaSaysHello(rc.Ae, rc.Rtc)
 	logger.Info("reached the line after riaSaysHello()") // REMOVE
