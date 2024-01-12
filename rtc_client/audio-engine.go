b72b6637be59f92ef752b3ad5f9ef33548e72455
Ananth Preetham <infinityp913@gmail.com>
2024-01-11 19:02:41 -0500

    changed to 80ms framesize

--- rtc_client/audio-engine.go
+++ rtc_client/audio-engine.go
@@ -23,7 +23,7 @@ import (
 const (
 	sampleRate  = stt.SampleRate // (16000)
 	channels    = 1              // decode into 1 channel since that is what whisper.cpp wants
-	frameSizeMs = 60
+	frameSizeMs = 80
 )
 
 var frameSize = channels * frameSizeMs * sampleRate / 1000
@@ -111,8 +111,8 @@ func NewAudioEngine(sttEngine *stt.Engine) (*AudioEngine, error) {
 	ae := &AudioEngine{
 		rtpIn:          make(chan *rtp.Packet),
 		mediaOut:       make(chan media.Sample),
-		pcm:            make([]float32, frameSize/3),
-		buf:            make([]byte, frameSize/3*2),
+		pcm:            make([]float32, frameSize/4),
+		buf:            make([]byte, frameSize/4*2),
 		dec:            dec,
 		enc:            enc,
 		sttEngine:      sttEngine,
@@ -175,7 +175,7 @@ func (a *AudioEngine) sendMedia(frames []internal.OpusFrame) {
 		sample := convertOpusToSample(f)
 		a.mediaOut <- sample
 		// this is important to properly pace the samples
-		time.Sleep(time.Millisecond * 60)
+		time.Sleep(time.Millisecond * 80)
 		if f.IsLastFrame {
 			a.mediaOut <- media.Sample{
 				Data: nil,
@@ -194,7 +194,7 @@ func (a *AudioEngine) SendMedia(frames []OpusFrame) {
 		sample := convertOpusToSampleNew(f)
 		a.mediaOut <- sample
 		// this is important to properly pace the samples
-		time.Sleep(time.Millisecond * 60)
+		time.Sleep(time.Millisecond * 80)
 	}
 	internal.Logger.Info("DEBUG: End of sendMedia")
 }
@@ -210,7 +210,7 @@ func (a *AudioEngine) SendMediaByteArr(byteArr []byte) {
 	}
 	a.mediaOut <- sample
 	// this is important to properly pace the samples
-	time.Sleep(time.Millisecond * 60)
+	time.Sleep(time.Millisecond * 80)
 	internal.Logger.Info("DEBUG: End of sendMedia")
 }
 
@@ -219,7 +219,7 @@ func convertOpusToSample(frame internal.OpusFrame) media.Sample {
 	return media.Sample{
 		Data:               frame.Data,
 		PrevDroppedPackets: 0, // FIXME support dropping packets
-		Duration:           time.Millisecond * 60,
+		Duration:           time.Millisecond * 80,
 	}
 }
 
@@ -227,7 +227,7 @@ func convertOpusToSampleNew(frame OpusFrame) media.Sample {
 	return media.Sample{
 		Data:               frame.Data,
 		PrevDroppedPackets: 0, // FIXME support dropping packets
-		Duration:           time.Millisecond * 60,
+		Duration:           time.Millisecond * 80,
 	}
 }
 
