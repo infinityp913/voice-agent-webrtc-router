ff572a4171d2342fdf4cfe5633b6274fd3c1f2f8
Ananth Preetham <infinityp913@gmail.com>
2024-03-18 12:44:25 -0400

    chanegd sleep before riasayshello to 2.8s

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -135,9 +135,9 @@ func main() {
 		logger.Fatal(err, "error creating offer")
 	} //NOV 28
 
-	time.Sleep(2500 * time.Millisecond)
+	// time.Sleep(2500 * time.Millisecond)
 	// time.Sleep(1500 * time.Millisecond)
-	// time.Sleep(2800 * time.Millisecond)
+	time.Sleep(2800 * time.Millisecond)
 
 	init_state := riaSaysHello(rc.Ae, rc.Rtc, client_id)
 	logger.Info("reached the line after riaSaysHello()") // REMOVE
