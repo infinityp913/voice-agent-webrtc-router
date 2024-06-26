e5b63fcdf0c9b7c055a437c5da936ca6d5c1eccf
Ananth Preetham <infinityp913@gmail.com>
2024-06-26 10:30:26 -0400

    prepending 5.5s silence to riasayshello

--- rtc_client/cmd/rtc-whisper-client.go
+++ rtc_client/cmd/rtc-whisper-client.go
@@ -632,7 +632,7 @@ func riaSaysHello(ae *rtc_client.AudioEngine, rtc *rtc_client.RTCConnection, cli
 			chunk.ChannelCount = 1
 
 			// data := make([]float32, 26850) // (26850 after adding 0.5s more ie 11025 (0.5*22050) more) prepending silence. this was 4800 (previously 38050, idk why we increased) and now increasing by 500ms ie 11025 samples (the duration of the pick up sound)
-			data := make([]float32, 159150) // india ring time = 7.08s, US ring time 4.19s. need to prepend silence of 6s more ie, 110250 more(22050*6 = 132300). So, 26850+132300=159150
+			data := make([]float32, 148125) // india ring time = 7.08s, US ring time 4.19s. need to prepend silence of 5.5s more ie, 121275 more(22050*5.5 = 121275). So, 26850+121275=148125
 			data = append(data, chunk.Data...)
 			chunk.Data = data
 
